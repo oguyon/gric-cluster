c167c9b34370a3de1b48ae9040e3fea45f37faf8
oguyon <oliv.guyon@gmail.com>
2026-09-15 18:17:01 -1000

    fix(build): guard immintrin.h header on non-x86 architectures in knn_reconstruct.c

--- src/gric-knn/knn_reconstruct.c
+++ src/gric-knn/knn_reconstruct.c
@@ -7,7 +7,9 @@
 #include "knn_reader.h"
 #include "shared/cli_colors.h"
 #include "../../shared/gric_bin_io.h"
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
 #include <immintrin.h>
+#endif
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
